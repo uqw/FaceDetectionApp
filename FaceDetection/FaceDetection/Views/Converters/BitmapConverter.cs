69467bba911cfa666eae6ba3538fb0c76709e1b2
user <user@NOTB5126>
2016-05-18 14:38:17 +0200

    blah

---
+++ FaceDetection/FaceDetection/Views/Converters/BitmapConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace FaceDetection.Views.Converters
+{
+    class BitmapConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                MemoryStream ms = new MemoryStream();
+                ((System.Drawing.Bitmap)value).Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+                BitmapImage image = new BitmapImage();
+                image.BeginInit();
+                ms.Seek(0, SeekOrigin.Begin);
+                image.StreamSource = ms;
+                image.EndInit();
+
+                return image;
+            }
+            catch(Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Error converting image: " + ex);
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FaceDetection/FaceDetectionForms/Form1.cs
@@ -0,0 +1,30 @@
+﻿
+using Emgu.CV;
+using System;
+using System.Windows.Forms;
+
+namespace FaceDetectionForms
+{
+    public partial class Form1 : Form
+    {
+        private Capture _capture;
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            try
+            {
+                _capture = new Capture();
+                 imageBox.Image = _capture.QueryFrame();
+            }
+            catch(Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Could not get capture: " + ex);
+            }
+            
+        }
+        
+    }
+}
